4d30771d2b6cde0eb729c0c159618b56bb6bee3c
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2024-01-14 18:23:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.6 MB (99.98%)

--- 0088-merge-sorted-array/solution.cpp
+++ 0088-merge-sorted-array/solution.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int j = m;
-        for(int i = 0 ; i < n ;i++)
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        for(int i = 0;i< n;i++)
         {
-            nums1[j] =  nums2[i];
-            j++;
-        }
+            nums1[m+i] = nums2[i];
+        }    
         sort(nums1.begin(),nums1.end());
-    }   
+    }
 };
+
+
+
