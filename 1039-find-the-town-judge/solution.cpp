e93bf524f1f6dc2c7e54534509183003fca07a46
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-10 05:42:47 +0000

    [LeetCode Sync] Runtime - 36 ms (19.40%), Memory - 73.3 MB (25.98%)

---
+++ 1039-find-the-town-judge/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+       vector<int> count(n+1,0);
+
+        for(auto t : trust)
+            {
+                int a = t[0];
+                int b = t[1];
+
+                count[a]--;
+                count[b]++;
+            }
+
+        for(int i = 1; i <= n;i++)
+            {
+                if(count[i] == n-1)
+                {
+                    return i;
+                }
+            }
+
+        return -1;
+    }
+};
