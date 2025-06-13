1ef5a997138ed69aaa8394767233fe8ffac68170
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-13 18:10:03 +0000

    [LeetCode Sync] Runtime - 44 ms (79.23%), Memory - 29.1 MB (48.19%)

--- 0015-3sum/solution.cpp
+++ 0015-3sum/solution.cpp
@@ -1,50 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // vector<vector<int>> arr;
-        // for(int i = 0 ; i < nums.size() ; i++)
-        // {
-        //     for(int j = i+1 ; j < nums.size() - 1 ; j++)
-        //     {
-        //         for(int k = i + 2 ; k < nums.size() - 2 ; k++)
-        //         {
-        //             if(nums[i]+nums[j]+nums[k] == 0)
-        //             {
-        //                 arr.insert(arr.end(),{nums[i],nums[j],nums[k]});
-        //             }
-        //         }
-        //     }
-        // }
-        // return arr;\
+    //     vector<vector<int>> arr;
+    //     for(int i = 0 ; i < nums.size() - 2 ; i++) 
+    //     {
+    //         for(int j = i + 1 ; j < nums.size() - 1 ; j++)
+    //         { 
+    //             for(int k = j + 1 ; k < nums.size() ; k++) 
+    //             {
+    //                 if(nums[i] + nums[j] + nums[k] == 0) 
+    //                 {
+    //                     vector<int> triplet = {nums[i], nums[j], nums[k]};
+    //                     sort(triplet.begin(), triplet.end());
+    //                     arr.push_back(triplet);
+    //                 }
+    //             }
+    //         }
+    //     }
 
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+    //     sort(arr.begin(), arr.end());
+    //     arr.erase(unique(arr.begin(), arr.end()), arr.end());
 
-        for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate
+    //     return arr;
+    
+    sort(nums.begin(),nums.end());
+    vector<vector<int>> arr;
 
-            int left = i + 1;
-            int right = n - 1;
+    for(int i = 0 ; i < nums.size() - 2; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        int left = i+1 , right = nums.size()-1;
+        while(left < right)
+        {
+        int sum = nums[i] + nums[left] + nums[right];
+        if(sum == 0)
+        {
+            arr.push_back({nums[i], nums[left], nums[right]});
 
-            while(left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[left] == nums[left + 1]) left++;  
-                    while(left < right && nums[right] == nums[right - 1]) right--; 
-                    left++;
-                    right--;
-                }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
-            }
+            while (left < right && nums[left] == nums[left + 1]) left++;
+            while (left < right && nums[right] == nums[right - 1]) right--;
+            
+            left++;
+            right--;
         }
-
-        return res;
+        else if(sum < 0)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+        }
+    }
+        return arr;
     }
 };
