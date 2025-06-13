2b79d558eab33fc25109e9cdfe1dab94546d87ec
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-13 14:07:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 27.2 MB (79.95%)

---
+++ 0344-reverse-string/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0 , right = s.size()-1;
+        int temp;
+        while(left < right)
+        {
+            temp = s[left]; 
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+
+            // swap(s[left++],s[right--]);
+            // reverse(s.begin(), s.end());
+        }
+    }
+};
