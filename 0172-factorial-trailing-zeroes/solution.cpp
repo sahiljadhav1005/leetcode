404083521e4205a090233de8728a0cd803bfaa6c
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-13 11:32:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (85.89%)

--- 0172-factorial-trailing-zeroes/solution.cpp
+++ 0172-factorial-trailing-zeroes/solution.cpp
@@ -1,19 +1,20 @@
-// class Solution {
-// public:
-//     int trailingZeroes(int n) {
-//         return n/5 + n/25 + n/125 + n/625 + n/3125;
-//     }
-// }; 
-
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int suma = 0;
-
-        for (int i=1; i<=log(n)/log(5); i++){
-            suma += n/pow(5,i);
+        int count = 0;
+        while(n >= 5)
+        {
+            count = count + n/5;
+            n = n / 5;
         }
+        return count;
 
-        return suma;
+        // int suma = 0;
+
+        // for (int i=1; i<=log(n)/log(5); i++){
+        //     suma += n/pow(5,i);
+        // }
+
+        // return suma;
     }
-}; 
+};
