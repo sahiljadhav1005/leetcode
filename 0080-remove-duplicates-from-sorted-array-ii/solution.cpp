5acb8e588e15ed8aae892d37f10daa1521dc157e
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-14 06:28:58 +0000

    [LeetCode Sync] Runtime - 6 ms (75.74%), Memory - 19.6 MB (55.82%)

--- 0080-remove-duplicates-from-sorted-array-ii/solution.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/solution.cpp
@@ -1,44 +1,17 @@
-// class Solution {
-// public:
-//     int removeDuplicates(vector<int>& nums)
-//     {
-//         int index = 1;
-//         int j = 1;
-//         for(int i = 1; i < nums.size(); i++)
-//         {
-//             if(nums[i-1] == nums[i] && j < 2)
-//             {
-//                 nums[index] = nums[i];
-//                 j++;
-//                 index++;
-//             }
-//             else if(nums[i-1] != nums[i])
-//             {
-//                 nums[index] = nums[i];
-//                 j = 1; // Reset the count of duplicates
-//                 index++;
-//             }
-//         }
-//         return index;
-//     }
-// };
-#include <vector>
-
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        if (nums.size() <= 2) {
-            return nums.size();
-        }
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 2) return nums.size();
 
-        int index = 2; // Start from the third element
-        for (int i = 2; i < nums.size(); ++i) {
-            if (nums[i] != nums[index - 2]) {
-                nums[index] = nums[i];
-                ++index;
+        int i = 2; // start from index 2
+
+        for (int j = 2; j < nums.size(); j++) {
+            if (nums[j] != nums[i - 2]) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        return index;
+
+        return i;
     }
 };
-
