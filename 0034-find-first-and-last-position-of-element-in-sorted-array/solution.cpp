a9f502af91c804625ee4a6347239f0ce23f3c172
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-16 07:30:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (86.43%)

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        // vector<int> res;
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     if(nums[i] == target)
+        //     {
+        //         res.push_back(i);
+        //     }
+        // }
+
+        // if (res.empty())
+        // {
+        //     return {-1, -1};
+        // }
+
+        // return {res.front(), res.back()};
+
+        // int first = -1, last = -1;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     if (nums[i] == target) {
+        //         if (first == -1) first = i;
+        //         last = i;
+        //     }
+        // }
+        // return {first, last};
+
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = 0;
+        int first = -1 , last = -1; 
+
+        while(start <= end)
+        {
+            mid = start + (end - start) / 2 ; 
+            if(target == nums[mid])
+            {
+                first = mid;
+                end = mid - 1;
+            }
+            else if(target > nums[mid])
+            {
+                start = start + 1;
+            }
+            else
+            {
+                end = end - 1;
+            }
+        }
+
+        start = 0 ;
+        end = nums.size() - 1;
+
+        while(start <= end)
+        {
+            mid = start + (end-start) / 2;
+            if(target == nums[mid])
+            {
+                last = mid;
+                start = mid + 1;
+            }
+            else if(target > nums[mid])
+            {
+                start = start + 1;
+            }
+            else
+            {
+                end = end - 1;
+            }
+        }
+
+        return {first,last};
+
+
+    }
+};
