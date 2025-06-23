e4dc25d4ac48f9d9a672e06510386c4e738995fb
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-23 17:19:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (17.70%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,6 +1,56 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+
+        int left = 0 ;
+        int right = nums.size() - 1;
+        int mid = 0;
+        int last = -1 , first = -1;
+
+        while(left <= right)
+        {
+            mid = left + (right -left) / 2;
+
+            if(nums[mid] == target)
+            {
+                first = mid;
+                right = mid - 1;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = nums.size() - 1;
+        mid = 0;
+
+        while(left <= right)
+        {
+            mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+            {
+                last = mid;
+                left = mid + 1;
+            }
+            else if(nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return{first,last};
+
         // vector<int> res;
         // for(int i = 0 ; i < nums.size() ; i++)
         // {
@@ -26,52 +76,50 @@ public:
         // }
         // return {first, last};
 
-        int start = 0;
-        int end = nums.size() - 1;
-        int mid = 0;
-        int first = -1 , last = -1; 
+        // int start = 0;
+        // int end = nums.size() - 1;
+        // int mid = 0;
+        // int first = -1 , last = -1; 
 
-        while(start <= end)
-        {
-            mid = start + (end - start) / 2 ; 
-            if(target == nums[mid])
-            {
-                first = mid;
-                end = mid - 1;
-            }
-            else if(target > nums[mid])
-            {
-                start = start + 1;
-            }
-            else
-            {
-                end = end - 1;
-            }
-        }
+        // while(start <= end)
+        // {
+        //     mid = start + (end - start) / 2 ; 
+        //     if(target == nums[mid])
+        //     {
+        //         first = mid;
+        //         end = mid - 1;
+        //     }
+        //     else if(target > nums[mid])
+        //     {
+        //         start = mid + 1;
+        //     }
+        //     else
+        //     {
+        //         end = mid - 1;
+        //     }
+        // }
 
-        start = 0 ;
-        end = nums.size() - 1;
+        // start = 0 ;
+        // end = nums.size() - 1;
 
-        while(start <= end)
-        {
-            mid = start + (end-start) / 2;
-            if(target == nums[mid])
-            {
-                last = mid;
-                start = mid + 1;
-            }
-            else if(target > nums[mid])
-            {
-                start = start + 1;
-            }
-            else
-            {
-                end = end - 1;
-            }
-        }
+        // while(start <= end)
+        // {
+        //     mid = start + (end-start) / 2;
+        //     if(target == nums[mid])
+        //     {
+        //         last = mid;
+        //         start = mid + 1;
+        //     }
+        //     else if(target > nums[mid])
+        //     {
+        //         start = mid + 1;
+        //     }
+        //     else
+        //     {
+        //         end = mid - 1;
+        //     }
+        // }
 
-        return {first,last};
-
-
+        // return {first,last};
     }
 };
