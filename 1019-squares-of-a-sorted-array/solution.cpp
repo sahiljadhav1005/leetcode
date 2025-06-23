ce9ebf28ea5e9ee8fc3effed3c37069a80b599f0
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-23 09:03:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 30.3 MB (63.66%)

--- 1019-squares-of-a-sorted-array/solution.cpp
+++ 1019-squares-of-a-sorted-array/solution.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i = 0 ; i < nums.size() ; i++)
-        {
-            nums[i] = nums[i] * nums[i];
-        }
-
-        sort(nums.begin(),nums.end());
-
-        return nums;
-
-        // int n = nums.size();
-        // vector<int> result(n);
-        // int left = 0, right = n - 1;
-        // int pos = n - 1;
-
-        // while (left <= right) {
-        //     int leftSq = nums[left] * nums[left];
-        //     int rightSq = nums[right] * nums[right];
-
-        //     if (leftSq > rightSq) {
-        //         result[pos--] = leftSq;
-        //         left++;
-        //     } else {
-        //         result[pos--] = rightSq;
-        //         right--;
-        //     }
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     nums[i] = nums[i] * nums[i];
         // }
 
-        // return result;
+        // sort(nums.begin(),nums.end());
+
+        // return nums;
+
+        int n = nums.size() ;
+        int left = 0 , right = n - 1;
+        int pos = n - 1;
+        vector<int> result(n);
+        while(left <= right)
+        {
+            int leftsq = nums[left] * nums[left];
+            int rightsq = nums[right] * nums[right];
+
+            if(leftsq > rightsq)
+            {
+                result[pos] = leftsq;
+                pos--;
+                left++;
+            }
+            else
+            {
+                result[pos] = rightsq;
+                pos--;
+                right--;
+            }
+        }
+
+        return result;
     }
 };
