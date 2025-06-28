cb8bb7d839cc6fd13e4632c6f38c2f0a02fcd9b8
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-06-28 10:04:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (90.70%)

---
+++ 1005-univalued-binary-tree/solution.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isUnival(TreeNode* root, int val) 
+    {
+        if (root == nullptr) 
+        {
+            return true;
+        }
+
+        if (root->val != val) 
+        {
+            return false;
+        }
+
+        return isUnival(root->left, val) && isUnival(root->right, val);
+    }
+
+    bool isUnivalTree(TreeNode* root) 
+    {
+        if (root == nullptr)
+        {
+            return true;
+        }
+        
+        return isUnival(root, root->val);
+    }
+};
