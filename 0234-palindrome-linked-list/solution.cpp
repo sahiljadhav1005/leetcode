e6d7f125f1666b452ef5db8dde2904be0800ffb0
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-07-25 08:22:26 +0000

    [LeetCode Sync] Runtime - 4 ms (53.76%), Memory - 118 MB (84.60%)

--- 0234-palindrome-linked-list/solution.cpp
+++ 0234-palindrome-linked-list/solution.cpp
@@ -11,42 +11,46 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) 
+        if(!head && !head->next)
         {
             return true;
         }
 
-        // Step 1: Find middle
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast->next && fast->next->next) 
+        // step 1 : find the middle of list
+
+        ListNode * slow = head;
+        ListNode * fast = head;
+
+        while(fast && fast->next)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        // Step 2: Reverse second half
-        ListNode* prev = nullptr;
-        ListNode* curr = slow->next;
-    
-        while (curr) 
+        // step 2 : reverse the half list
+
+        ListNode * curr = slow;
+        ListNode * prev = nullptr;
+
+        while(curr)
         {
-            ListNode* temp = curr->next;
+            ListNode * temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
         }
 
-        // Step 3: Compare both halves
-        ListNode* first = head;
-        ListNode* second = prev;
-        while (second) 
+        // step 3 : compare both half
+
+        ListNode * first = head;
+        ListNode * second = prev;
+
+        while(second)
         {
-            if (first->val != second->val)
+            if(first->val != second->val)
             {
                 return false;
             }
-
             first = first->next;
             second = second->next;
         }
