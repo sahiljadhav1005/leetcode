088af675bcb27e7465821bdfec8a679d1d1e9134
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-08-07 16:20:21 +0000

    [LeetCode Sync] Runtime - 13 ms (39.29%), Memory - 17.5 MB (27.21%)

---
+++ 0139-word-break/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin() , wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1 , false);
+        dp[0] = true;
+
+        for(int i = 1 ; i <= n ; ++i)
+        {
+            for(int j = 0 ; j < i ; ++j)
+            {
+                string word = s.substr(j , i - j);
+                if(dp[j] && dict.count(word))
+                {
+                    dp[i] = true;
+                    break;
+                } 
+            }
+        }
+
+        return dp[n];
+    }
+};
