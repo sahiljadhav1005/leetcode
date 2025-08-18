3bd5b52a38486d751fcc3f535bad435e119f2ffd
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-08-18 14:33:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55 MB (48.43%)

---
+++ 0041-first-missing-positive/solution.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) 
+        {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) 
+            {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) 
+        {
+            if (nums[i] != i + 1) 
+            {
+                return i + 1;
+            }
+        }
+        
+        return n + 1; 
+
+        // unordered_set<int> s(nums.begin(), nums.end());
+        
+        // int n = nums.size();
+        
+        // for (int i = 1; i <= n+1; i++) 
+        // {
+        //     if (!s.count(i)) 
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return n+1;
+    }
+};
