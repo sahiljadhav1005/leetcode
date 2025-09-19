18570d8cf6fd0996f9d1a13d17593e9a70c823b6
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-09-19 20:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (59.35%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -5,38 +5,59 @@ public:
         //                      1       2
         //                      3       4
 
-        vector<int> res1;
-        vector<int> res2;
+        // method 1 
+        // vector<int> res1;
+        // vector<int> res2;
+        // vector<int> res;
+        // sort(nums.begin() , nums.end());
+
+        // for(int i = 0 ; i < nums.size() ; i++)
+        // {
+        //     if(i % 2 == 1)
+        //     {
+        //         res1.push_back(nums[i]);
+        //     }
+        //     else
+        //     {
+        //         res2.push_back(nums[i]);
+        //     }
+        // }
+
+        // for(int i = 0 ; i < res1.size() && i < res2.size() ; i++)
+        // {
+        //     res.push_back(res1[i]);
+        //     res.push_back(res2[i]);
+        // }
+
+        // if (res1.size() > res2.size()) 
+        // {
+        //     res.push_back(res1.back());
+        // } 
+        // else if (res2.size() > res1.size()) 
+        // {
+        //     res.push_back(res2.back());
+        // }
+
+        // return res;
+
+        // method 2 
+
+        sort(nums.begin(), nums.end());
+
         vector<int> res;
-        sort(nums.begin() , nums.end());
 
-        for(int i = 0 ; i < nums.size() ; i++)
+        for (int i = 0; i < nums.size(); i += 2) 
         {
-            if(i % 2 == 1)
+            if (i + 1 < nums.size()) 
             {
-                res1.push_back(nums[i]);
-            }
-            else
+                res.push_back(nums[i + 1]); // B's number
+                res.push_back(nums[i]);     // A's number
+            } 
+            else 
             {
-                res2.push_back(nums[i]);
+                res.push_back(nums[i]);     // If odd element remains
             }
         }
-
-        for(int i = 0 ; i < res1.size() && i < res2.size() ; i++)
-        {
-            res.push_back(res1[i]);
-            res.push_back(res2[i]);
-        }
-
-        if (res1.size() > res2.size()) 
-        {
-            res.push_back(res1.back());
-        } 
-        else if (res2.size() > res1.size()) 
-        {
-            res.push_back(res2.back());
-        }
-
         return res;
     }
 };
