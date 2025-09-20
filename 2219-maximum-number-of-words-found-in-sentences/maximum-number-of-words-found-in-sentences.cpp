09fd6df38a5a4f3366d6b9a7143657c9dc614da3
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-09-20 19:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (34.92%) - LeetSync

--- 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
+        // method 1
+
+        // int max1 = 0;
+        // for (int i = 0; i < sentences.size(); i++) 
+        // {
+        //     stringstream ss(sentences[i]);
+        //     int  count = 0 ;
+        //     string word;
+        //     while (ss >> word) 
+        //     {
+        //         count++;
+        //     }
+
+        //     max1 = max(count, max1);
+        // }
+
+        // return max1;
+
+        // method 2
+
         int max1 = 0;
-        for (int i = 0; i < sentences.size(); i++) 
+
+        for (string s : sentences) 
         {
-            stringstream ss(sentences[i]);
-            int  count = 0 ;
-            string word;
-            while (ss >> word) 
+            int count = 1; // at least 1 word
+
+            for (char c : s) 
             {
-                count++;
+                if (c == ' ') count++;
             }
 
-            max1 = max(count, max1);
+            max1 = max(max1, count);
         }
 
         return max1;
