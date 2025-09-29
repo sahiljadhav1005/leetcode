e54c3f902e12ca585442f49a0cfc63d996f17db7
SAHIL JADHAV <121169630+sahiljadhav1005@users.noreply.github.com>
2025-09-29 17:32:20 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (60.49%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int res = 0, d = 2;
+        while (n > 1) 
+        {
+            while (n % d == 0) 
+            {
+                res += d;
+                n /= d;
+            }
+            d++;
+        }
+        return res;
+    }
+};
